a19287bc73ceba724ce6d9af29cf8740e5007633
Opi Danihelka <opicacek@seznam.cz>
2013-08-12 04:10:01 +0200

    Save commit before changes for AppFrog.

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ var en = new engine.Engine()
 // accessible from the web
 var fileServer = new static.Server('./');
 
-
+//TODO required for Heroku
+/*
 io.configure(function () { 
 	io.set("transports", ["xhr-polling"]); 
 	io.set("polling duration", 10); 
 });
+*/
 
 // This is the port for our web server.
 // you will need to go to http://localhost:8080 to see it
